28be6159439f888992c8ef1559a523017a43b86a
acmmm-23 <130882699+acmmm-23@users.noreply.github.com>
2023-04-16 15:01:07 +0800

    Add files via upload

---
+++ cwtvlad/models/__init__.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+from .vgg import *
+from .mobilenetv3 import *
+from .cwtvlad import *
+
+
+__factory = {
+    'vgg16': vgg16,
+    'vgg19': vgg19,
+    'mobilenetv3_large': mobilenetv3_large,
+    'mobilenetv3_small': mobilenetv3_small,
+    'cwtvlad': CWTVLAD,
+    'embednet': EmbedNet,
+    'embedregiontrans': EmbedRegionTrans,
+}
+
+
+def names():
+    return sorted(__factory.keys())
+
+
+def create(name, *args, **kwargs):
+    """
+    Create a model instance.
+
+    Parameters
+    ----------
+    name : str
+        Model name. Can be one of 'mobilenetv3_large', 'mobilenetv3_small', 'transgvlad',
+        'embednet', 'embedregiontrans'.
+    """
+    if name not in __factory:
+        raise KeyError("Unknown model:", name)
+    return __factory[name](*args, **kwargs)
